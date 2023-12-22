2962a7f34a7640071878181cc4b4ccbd35ca5e14
rahimov27 <89564054+rahimov27@users.noreply.github.com>
2023-12-22 23:20:24 +0100

    Delete pyramid.py

--- pyramid.py
+++
@@ -1,8 +0,0 @@
-for i in range(0,10):
-    for j in range(i + 1):
-        print("*", end=" ")
-    print()
-for e in range(10,0,-1):
-    for k in range(e-1):
-        print("*", end=" ")
-    print()
